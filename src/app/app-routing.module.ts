7c74b81fa83067a4cf7cbbbf6e1d5d1d42d22c2c
abdur-rehman-ali <alijoyia911@gmail.com>
2022-06-04 18:19:32 +0500

    Register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HistoryComponent } from './history/history.component';
 import { FormComponent } from './form/form.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 const routes: Routes = [
   { path:'history' , component:HistoryComponent},
   { path:'form' , component:FormComponent},
+  { path:'register' , component:RegistrationComponent},
 ];
 
 @NgModule({
