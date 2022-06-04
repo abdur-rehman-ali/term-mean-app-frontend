b8d0e4aedf62d707d2da340d39b50980db9174f4
abdur-rehman-ali <alijoyia911@gmail.com>
2022-06-04 16:45:08 +0500

    Component for history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'history' , component:HistoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
